59a62809e763910cb18dda4384233227288ee84a
Chinmay Nayak <chinmay.nayak@pwc.com>
2020-01-16 16:27:15 +0530

    Finished creating the Localization Framework.

---
+++ LocalizationFramework/LocalizationFramework/StringLocalization.swift
@@ -0,0 +1,18 @@
+//
+//  StringLocalization.swift
+//  LocalizationFramework
+//
+//  Created by Chinmay Nayak on 16/01/20.
+//  Copyright © 2020 Demo. All rights reserved.
+//
+
+import Foundation
+
+public class StringLocalization {
+    
+    private init () {}
+    
+    public static func localize() -> String {
+        return "Will be implemented shortly."
+    }
+}
